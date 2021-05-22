dc47919eb7005ecf1410fbb7108973909237eada
alswns <49472611+alswns@users.noreply.github.com>
2021-05-22 19:25:03 +0900

    emotion_classfication

--- BluePrint/music.py
+++ BluePrint/music.py
@@ -2,9 +2,10 @@ from flask import Flask,request,jsonify,abort,Blueprint,make_response,Response
 # from .dialogflow import Dialogflow
 from flask_restplus import Namespace,Resource
 from apis.spotify import Spotify
+from apis.emotion_classfication_model import emotion_analysis
 import io
 music_api = Namespace('music', description='Music APIs')
-
+model=emotion_analysis('./models/emotion_classfication.h5',50432)
 spotify=Spotify()
 
 @music_api.route('/')
@@ -13,7 +14,7 @@ class Chat(Resource):
     @music_api.doc(responses={200: 'Success', 404: 'Parameter is empty', 500: 'Server Error'})
     def get(self):
         #data=request.args
-
+        print(model.pred('오늘 기분이 너무 좋아'))
         music=Spotify()
         items=music.getMusic('2G4AUqfwxcV1UdQjm2ouYr',9)
         
---
+++ apis/emotion_classfication_model.py
@@ -0,0 +1,40 @@
+
+from keras.models import load_model
+from transformers import PreTrainedTokenizerFast
+import numpy as np
+U_TKN = '<usr>'
+S_TKN = '<sys>'
+BOS = '</s>'
+EOS = '</s>'
+MASK = '<unused0>'
+SENT = '<unused1>'
+PAD = '<pad>'
+
+TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
+            bos_token=BOS, eos_token=EOS, unk_token='<unk>',
+            pad_token=PAD, mask_token=MASK) 
+
+tok = TOKENIZER
+
+class emotion_analysis():
+  def __init__(self,path,max_ids):
+    self.model=load_model(path)
+    self.max_ids=max_ids
+  def pred(self,words):
+    data=self.word_to_encoded(words)
+    pre=self.model.predict(data)
+    return np.argmax(pre)
+
+  def word_to_encoded(self,words):
+    toks=tok.tokenize(words)
+    tok_list=token=tok.convert_tokens_to_ids(toks)
+    zero_list=np.zeros(self.max_ids+1)
+
+    for i in tok_list:
+      zero_list[i]=1
+      
+    x_data=[]
+    x_data.append(zero_list)
+    x_data=np.array(x_data)
+    return x_data
+  
